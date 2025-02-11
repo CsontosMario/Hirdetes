8735f9e660fa91cb3cd758c43f96eeccd9c7b08f
Vmarci03 <vekony.marcell-2020@keri.mako.hu>
2025-02-11 12:19:06 +0100

    telefonszám hozzáadása az adatbázishoz

--- db/hirdetes.sql
+++ db/hirdetes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 10. 13:56
+-- Létrehozás ideje: 2025. Feb 11. 12:18
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.1.17
 
@@ -99,7 +99,7 @@ CREATE TABLE `regisztracio` (
   `felhasznaloID` int(11) NOT NULL,
   `azonosito` varchar(50) NOT NULL,
   `nev` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `telefonszam` varchar(20) DEFAULT NULL,
   `jelszo` varchar(50) NOT NULL,
   `iranyitoszam` int(11) NOT NULL,
   `telepules` varchar(50) NOT NULL,
@@ -111,17 +111,16 @@ CREATE TABLE `regisztracio` (
 -- A tábla adatainak kiíratása `regisztracio`
 --
 
-INSERT INTO `regisztracio` (`felhasznaloID`, `azonosito`, `nev`, `email`, `jelszo`, `iranyitoszam`, `telepules`, `cim`, `orszag`) VALUES
-(1, 'vekonymarcell', 'Vékony Marcell', 'vekony.marcell-2020@keri.mako.hu', '1234aA', 6900, 'Makó', 'Posta.u. 1', 'Magyarország'),
-(2, 'csontosmariodavid', 'Csontos Márió Dávid', 'csontos.mariodavi-2020@keri.mako.hu', '1234aA', 6915, 'Csanádalberti', 'Munkácsy Mihály utca 23.', 'Magyarország'),
-(3, 'raikouusagi', 'Raikou Usagi', 'villamnyul7777@gmail.com', '1234aA', 6915, 'Csanádalberti', 'Dózsa György utca 3.', 'Magyarország'),
-(4, 'szabomario', 'Szabó Márió', 'szabomario64@gmail.com', '1234aA', 6900, 'Makó', 'Ady Endre u 1.', 'Magyarország'),
-(5, 'kissandrea', 'Kiss Andrea', 'kissandrea@gmail.com', '1234aA', 6900, 'Makó', 'Kossuth L utca 46.', 'Magyarország'),
-(6, 'nagyakos', 'Nagy Ákos', 'akosnagy@gmail.com', '1234aA', 6915, 'Csanádalberti', 'Petőfi Sándor utca 3.', 'Magyarország'),
-(7, 'nyaridaniel', 'Nyári Dániel', 'nyaridaniel@gmail.com', '1234aA', 6900, 'Makó', 'ukbuk.u. 1', 'Magyarország'),
-(8, 'frankcastle', 'Frank Castle', 'frankcas@gmail.com', '1234aA', 6900, 'New York', 'street.u 7.', 'United States'),
-(9, 'petecastallengtion', 'Pete Castallengtion', 'petecas@gmail.com', '1234aA', 10000, 'New York', 'Park Avenue', 'United States'),
-(10, 'soujiseta', 'Souji Seta', 'soujiseta@gmail.com', '1234aA', 777, 'Fuefuki, Yamanashi', 'Yamanashi Prefectural Rte 313', 'Japan');
+INSERT INTO `regisztracio` (`felhasznaloID`, `azonosito`, `nev`, `telefonszam`, `jelszo`, `iranyitoszam`, `telepules`, `cim`, `orszag`) VALUES
+(1, 'vekonymarcell', 'Vékony Marcell', NULL, '1234aA', 6900, 'Makó', 'Posta.u. 1', 'Magyarország'),
+(2, 'csontosmariodavid', 'Csontos Márió Dávid', NULL, '1234aA', 6915, 'Csanádalberti', 'Munkácsy Mihály utca 23.', 'Magyarország'),
+(3, 'raikouusagi', 'Raikou Usagi', NULL, '1234aA', 6915, 'Csanádalberti', 'Dózsa György utca 3.', 'Magyarország'),
+(4, 'szabomario', 'Szabó Márió', NULL, '1234aA', 6900, 'Makó', 'Ady Endre u 1.', 'Magyarország'),
+(5, 'kissandrea', 'Kiss Andrea', NULL, '1234aA', 6900, 'Makó', 'Kossuth L utca 46.', 'Magyarország'),
+(6, 'nagyakos', 'Nagy Ákos', NULL, '1234aA', 6915, 'Csanádalberti', 'Petőfi Sándor utca 3.', 'Magyarország'),
+(7, 'nyaridaniel', 'Nyári Dániel', NULL, '1234aA', 6900, 'Makó', 'ukbuk.u. 1', 'Magyarország'),
+(8, 'frankcastle', 'Frank Castle', NULL, '1234aA', 6900, 'New York', 'street.u 7.', 'United States'),
+(9, 'petecastallengtion', 'Pete Castallengtion', NULL, '1234aA', 10000, 'New York', 'Park Avenue', 'United States');
 
 -- --------------------------------------------------------
 
@@ -180,7 +179,7 @@ INSERT INTO `zoldsegek_hu` (`termekID`, `kep`, `megnevezes`, `leiras`, `ar`, `ki
 (4, 'spinach.jpg', 'Spenót', 'Közepes nagyságú levelek. Magyar termék.', 500, 'csomagonként'),
 (5, 'spring_onion.jpg', 'Újhagyma', 'Hosszúkás fejű, alkalmas friss fogyasztásra és salátákhoz. Magyar termék.', 250, 'csomagonként'),
 (6, 'corn.jpg', 'Kukorica (Csemege)', 'Közepes szemű, édes, főzésre kitűnő. Magyar termék.', 200, 'darabonként'),
-(7, 'pea.jpg', 'Zöldborsó (Zsuzsi)', 'Közepes nagyságú szemek, zsengék és alkalmas levesekhez, főzelékekhez, salátákhoz. Magyar termék.', 1000, 'ömlesztve'),
+(7, 'pea.jpg', 'Zöldborsó (Zsuzsi)', 'Közepes nagyságú szemek, zsengék és alkalmas levesekheez, főzelékekhez, salátákhoz. Magyar termék.', 1000, 'ömlesztve'),
 (8, 'kohlrabi.jpg', 'Karalábé', 'Zsenge, fehér. Magyar termék.', 450, 'darabonként'),
 (9, 'pepper.jpg', 'Paprika (Fehérözön)', 'Húsos, közepes nagyságú, friss fogyasztásra és főzésre egyaránt alkalmas. Magyar termék.', 800, 'ömlesztve'),
 (10, 'cauliflower.jpg', 'Karfiol', 'Nagyrózsájú, alkalmas levesek, főzelékek és számtalan étel készítésére. Magyar termék.', 800, 'fejenként');
@@ -199,22 +198,6 @@ CREATE TABLE `zoldsegek_jp` (
   `ar` int(10) NOT NULL,
   `kiszereles` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `zoldsegek_jp`
---
-
-INSERT INTO `zoldsegek_jp` (`termekID`, `kep`, `megnevezes`, `leiras`, `ar`, `kiszereles`) VALUES
-(1, 'potato.jpg', 'じゃがいも', '小麦粉含有量が中程度で、パン作りや調理に適しています。ハンガリー製品。', 1000, '大量に'),
-(2, 'carrot.jpg', 'キャロット', '滑らかな円筒形で、長さは中程度。ハンガリー製品。', 400, '大量に'),
-(3, 'sorrel.jpg', 'スイバ', 'ガーデンスイバ;葉が丈夫です。 ハンガリー製品。', 400, 'パッケージあたり'),
-(4, 'spinach.jpg', 'ほうれん草', '中くらいの大きさの葉。ハンガリー製品。', 500, 'パッケージあたり'),
-(5, 'spring_onion.jpg', 'ネギ', '細長い頭部を持ち、生食やサラダに適しています。ハンガリー製品。', 250, 'パッケージあたり'),
-(6, 'corn.jpg', 'コーン（デザート）', '中サイズで甘く、料理に最適です。ハンガリー製品。', 200, '一つ一つ'),
-(7, 'pea.jpg', 'グリーンピース', '中粒で柔らかく、スープ、野菜、サラダに適しています。ハンガリー製品。', 1000, '大量に'),
-(8, 'kohlrabi.jpg', 'コールラビ', '長男、白人。ハンガリー製品。', 450, '一つ一つ'),
-(9, 'pepper.jpg', 'パプリカ', '肉質が厚く、中型で、生食にも調理にも適しています。ハンガリー製品。', 800, '大量に'),
-(10, 'cauliflower.jpg', 'カリフラワー', '大きなロゼットが付いており、スープ、野菜、数え切れないほどの料理を作るのに適しています。ハンガリー製品。', 800, '頭ごとに');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -272,7 +255,7 @@ ALTER TABLE `zoldsegek_jp`
 -- AUTO_INCREMENT a táblához `regisztracio`
 --
 ALTER TABLE `regisztracio`
-  MODIFY `felhasznaloID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `felhasznaloID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT a táblához `zoldsegek_en`
@@ -290,7 +273,7 @@ ALTER TABLE `zoldsegek_hu`
 -- AUTO_INCREMENT a táblához `zoldsegek_jp`
 --
 ALTER TABLE `zoldsegek_jp`
-  MODIFY `termekID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `termekID` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
