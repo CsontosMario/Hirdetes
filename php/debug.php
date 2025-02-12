ccbc25a06a7d8accd4452efd96f170b2079ee2cd
odrya-keri-mako <odry.attila@keri.mako.hu>
2025-02-12 13:19:33 +0100

    tanári konzultáció: adatbázis, nyelv

--- php/debug.php
+++ php/debug.php
@@ -1,6 +1,14 @@
 <?php
 declare(strict_types=1);
 
-$_POST['data'] = '{"nev":"Vékony Marcell","jelszo":"1234aA"}';
+$_POST['data'] = '{
+    "nev": "sasasasa",
+    "email": "odry.attilac@keri.mako.hu",
+    "jelszo": "1234Aa",
+    "iranyitoszam": "233232",
+    "telepules": "SSSS",
+    "cim": "dffdfdfd",
+    "orszag": "sdds"
+}';
 
-require_once('felhasznalo.php');
+require_once('register.php');
