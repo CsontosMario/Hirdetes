75f7323c4134b40bae865a9a69e868f9514e2df2
CsontosMario <csontos.mariodavi-2020@keri.mako.hu>
2025-04-01 11:38:10 +0200

    Kosár fejlesztése.

--- php/toCart.php
+++ php/toCart.php
@@ -2,13 +2,13 @@
 
 require_once("../../common/php/environment.php");
 
-$db = new Database();
-
 $args=Util::getArgs();
+
+$db = new Database();
 
 $query= $db->preparateInsert("kosarelemek", $args);
 
-$result=$db->execute($query);
+$result=$db->execute($query, array_values($args));
 
 $db = null;
 
