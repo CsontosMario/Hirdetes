4c8105bb23da1782b91afc067f05ea564f163998
CsontosMario <csontos.mariodavi-2020@keri.mako.hu>
2025-04-16 08:29:32 +0200

    Profil változtatásához php készítése.

--- php/profile_change.php
+++ php/profile_change.php
@@ -2,3 +2,22 @@
 
 require_once("../../common/php/environment.php");
 
+$args = Util::getArgs();
+
+$db = new Database();
+
+$query = "UPDATE `regisztracio` 
+             SET `email`=:email,
+                 `nev`=:nev,
+                 `telefonszam`=:telefonszam,
+                 `iranyitoszam`=:iranyitoszam,
+                 `telepules`=:telepules,
+                 `cim`=:cim,
+                 `orszag`=:orszag 
+           WHERE `felhasznaloID` = :felhasznaloID";
+
+$result = $db->execute($query, $args);
+
+$db = null;
+
+Util::setResponse($result);
